f264e65e576bf38134e9f719e8f3f53497ebf465
StudyForZX <479431650@qq.com>
2017-08-17 19:12:06 +0800

    modify

--- manual/LanguageReference/ClassesAndObjects/basic.php
+++ manual/LanguageReference/ClassesAndObjects/basic.php
@@ -1,4 +1,27 @@
 <?php 
+
+//每个类的定义都以关键字class开头，后面跟着类名，后面跟着一对大括号，里面包含类的属性与方法的定义。
+// 类名可以是任何非PHP保留字的合法标签。一个合法类名以字母或者下划线开头，后面跟着若干字母，数字或下划线。以正则表达式表示为：[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*
+// 一个类可以包含有属于自己的常量，变量（称为属性）以及函数（成为方法）。
+
+/******************************************************/
+
+// 当一个方法在类的内部调用时，有一个可用的伪变量$this。$this 是一个到主叫对象的引用（通常是该方法所从属的对象，但如果是从第二个对象静态调用时也可能是另一个对象。）
+
+class SimpleClass
+{
+	//属性声明
+	public $val = 'a default value';
+
+	//方法声明
+	public function displayVar()
+	{
+		echo $this->var;
+	}
+}
+
+
+/******************************************************/
 class A
 {
     function foo()
@@ -25,6 +48,8 @@ $a = new A();
 $a->foo();
 
 A::foo();
+
+/******************************************************/
 /*
 Strict Standards: Non-static method A::foo() should not be called statically in D:\phpStudy\WWW\PHP\manual\ClassAndObject\basic.php on line 27
 $this is not defined.
