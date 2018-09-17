c2cea46c01446d692e0b5eb02ff5d93a08f951fd
StudyForZX <479431650@qq.com>
2018-09-17 10:23:52 +0800

    git add .

--- daily/manual/Control_Structures/goto.php
+++ daily/manual/Control_Structures/goto.php
@@ -26,12 +26,12 @@ end:echo "j hit 17";
 
 /***************/
 
-goto loop;
-for ($i=0, $j=50; $i < 100; $i++) { 
-	while ($j--) {
-		loop:
-	}
-}
-echo "$i = $i";
+// goto loop;
+// for ($i=0, $j=50; $i < 100; $i++) { 
+// 	while ($j--) {
+// 		loop:
+// 	}
+// }
+// echo "$i = $i";
 
 #Fatal error: 'goto' into loop or switch statement is disallowed in D:\phpStudy\WWW\PHP\manual\Control_Structures\goto.php on line 29
